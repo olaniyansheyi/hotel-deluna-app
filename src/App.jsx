4b819bde683aabc018e4934fddcd200b026246f4
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-14 13:47:40 +0100

    new feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,23 @@ const H1 = styled.h1`
   font-weight: 600;
 `;
 
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.3rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  margin: 20px;
+  cursor: pointer;
+`;
+
 function App() {
   return (
     <div>
       <H1>Hello World</H1>
+      <Button>Check in</Button>
     </div>
   );
 }
