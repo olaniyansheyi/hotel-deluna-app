273c2a7107a7050a2fece9bc770ebb470ee05855
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-14 13:36:28 +0100

    new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <H1>Hello World</H1>
+    </div>
+  );
 }
 
 export default App;
