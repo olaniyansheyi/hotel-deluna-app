17c685dd0ddacea409061da34c6b631fa5486ba6
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-16 00:27:46 +0100

    new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,7 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.div`
-  padding: 20px;
-`;
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">Hello World</Heading>
-
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button>Check in</Button>
-
-              <Button variation="secondary" size="small">
-                Check out
-              </Button>
-            </div>
-          </Row>
-          <Row>
-            <Heading to="h3">Form</Heading>
-            <Input
-              type="Number"
-              placeholder="
-      number of guests"
-            ></Input>
-            <Input
-              type="Number"
-              placeholder="
-      number of guests"
-            ></Input>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
-  );
+  return <BrowserRouter></BrowserRouter>;
 }
 
 export default App;
