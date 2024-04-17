fd3d7f1577304b00226cb2b8b4ff6fe87333d02d
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-17 20:59:57 +0100

    new feature

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://ablkdixvjqytwgxmyxxf.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFibGtkaXh2anF5dHdneG15eHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzNzQ2MTYsImV4cCI6MjAyODk1MDYxNn0.WettjNl_Y1Qv2YmrtqL7yfF6dnl19kG8xKL9ApSN-4A";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
