0ec669c9898b231b4f51788fedbc3cb749286c33
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-17 21:14:24 +0100

    new feature

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCarbins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(function () {
+    getCarbins().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+export async function getCarbins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("cabins could not be loaded");
+  }
+
+  return data;
+}
