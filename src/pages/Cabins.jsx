67187ac53493fba7fc53aeeb54f05a52d222e92e
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-18 19:42:47 +0100

    new feature

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCarbins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
   // useEffect(function () {
   //   getCarbins().then((data) => console.log(data));
   // }, []);
+
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <>
@@ -18,6 +21,11 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
