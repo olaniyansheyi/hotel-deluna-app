7ebe6b41d3ab76065a6a01cae29c38b3aa9d80a1
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-27 21:40:25 +0100

    new feature

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,9 +1,6 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
-import Button from "../ui/Button";
-import { useState } from "react";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
 import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
