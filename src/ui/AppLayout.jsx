c46c4bd34b1286f2512e51779dfa6fb63b14b646
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-28 21:16:46 +0100

    new feature

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import SideBar from "./SideBar";
 import Header from "./Header";
 import styled from "styled-components";
+import Button from "./Button";
 
 const StyledAppLayout = styled.div`
   display: grid;
