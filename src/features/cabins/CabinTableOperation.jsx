c3996b188db6114e14ddc4660d5ca76f558539b3
sheymantech <olaniyansheyi1704@gmail.com>
2024-05-01 19:26:25 +0100

    new feature

---
+++ src/features/cabins/CabinTableOperation.jsx
@@ -0,0 +1,7 @@
+import TableOperations from "../../ui/TableOperations";
+
+function CabinTableOperation() {
+  return <CabinTableOperation></CabinTableOperation>;
+}
+
+export default CabinTableOperation;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import SideBar from "./SideBar";
 import Header from "./Header";
 import styled from "styled-components";
-import Button from "./Button";
 
 const StyledAppLayout = styled.div`
   display: grid;
