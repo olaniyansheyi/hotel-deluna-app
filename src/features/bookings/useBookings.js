99f15bf0ee59ad824ad65ce2b9b59c48b30c7156
sheymantech <olaniyansheyi1704@gmail.com>
2024-05-02 13:07:16 +0100

    new feature

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+
+  const filteredValue = searchParams.get("status");
+  const filter =
+    !filteredValue || filteredValue === "all"
+      ? null
+      : { field: "status", value: filteredValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
     queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryFn: () => getBookings({ filter }),
   });
   return { isLoading, error, bookings };
 }
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,10 +1,16 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select("*, cabins(name), guests(fullName, email)");
+
+  // FILTER
+
+  if (filter !== null) query = query.eq(filter.field, filter.value);
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
